ee3c637cea190d47411ed334737803d161a79eee
Bambang <bambang.kanayama@gmail.com>
2024-10-29 19:52:26 +0700

    create Header and install react-icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Home from "./Pages/Home";
-import About from "./Pages/About";
+import Hrga from "./Pages/Hrga";
+import Finance from "./Pages/Finance";
+import Sales from "./Pages/Sales";
 import Project from "./Pages/Project";
 import Login from "./Pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -12,7 +14,9 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
+        <Route path="/Hrga" element={<Hrga />} />
+        <Route path="/Finance" element={<Finance />} />
+        <Route path="/Sales" element={<Sales />} />
         <Route path="/Project" element={<Project />} />
         <Route path="/Login" element={<Login />} />
       </Routes>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,25 +1,44 @@
-import { Navbar } from "flowbite-react";
+import { Button, Navbar } from "flowbite-react";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { CgLogIn } from "react-icons/cg";
+import { FaMoon } from "react-icons/fa";
 
 export default function Header() {
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
       <Navbar.Brand as={Link} href="#">
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          DASHBOARD MANAGEMENT
+        <span className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+          DASHBOARD
         </span>
       </Navbar.Brand>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-9" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Button gradientDuoTone="purpleToBlue" className="text-sm">
+          <CgLogIn className="mr-2 size-4 self-center" />
+          Login
+        </Button>
+      </div>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="#" active>
+        <Navbar.Link href="/" active={path === "/"}>
           Home
         </Navbar.Link>
-        <Navbar.Link as={Link} href="#">
-          About
+        <Navbar.Link href="/Hrga" active={path === "/Hrga"}>
+          HRGA
         </Navbar.Link>
-        <Navbar.Link href="#">Project</Navbar.Link>
-        <Navbar.Link href="#">Login</Navbar.Link>
+        <Navbar.Link href="/Finance" active={path === "/Finance"}>
+          Finance
+        </Navbar.Link>
+        <Navbar.Link href="/Sales" active={path === "/Sales"}>
+          Sales
+        </Navbar.Link>
+        <Navbar.Link href="/Project" active={path === "/Project"}>
+          Project
+        </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
