093a96ad0dc204bc2648fa2c46dec470cecc20ea
Bambang <bambang.kanayama@gmail.com>
2024-10-29 20:52:37 +0700

    create connection to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(`Server is connected to mongoDB`);
+  })
+  .catch((error) => {
+    console.log("error connection to database");
+  });
 
 app.listen(3000, () => {
   console.log("Server is ready..");
 });
+
+//MONGO_URI = mongodb+srv://gayaruangkita:zQKiQOEzmG7NvMf3@cluster0.luabs.mongodb.net/Kanayama?retryWrites=true&w=majority&appName=Cluster0
